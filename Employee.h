91762c68707780c1c3211d8151bf8c7b31489c6b
unknown <m.b.cole300@gmail.com>
2021-11-24 15:05:18 -0500

    Employee Class completed

---
+++ Employee.h
@@ -0,0 +1,89 @@
+#ifndef EMPLOYEE_H
+#define EMPLOYEE_H
+
+#include <iostream>
+using namespace std;
+
+class Employee{
+    
+    protected:
+        int EmployeeId;
+        string Name;
+        float RatePerHour;
+        float Salary;
+        string Currency;
+
+    public:
+        Employee(){
+
+            EmployeeId = 0;
+            Name = "";
+            RatePerHour = 0.0f;
+            Salary = 0.0f;
+            Currency = "";
+        }
+
+        Employee(int employeeId, string name, float ratePerHour, float salary, string currency){
+
+            EmployeeId = employeeId;
+            Name = name;
+            RatePerHour = ratePerHour;
+            Salary = salary;
+            Currency = currency;
+
+        }
+
+        Employee(Employee &employee){
+
+            EmployeeId = employee.EmployeeId;
+            Name = employee.Name;
+            RatePerHour = employee.RatePerHour;
+            Salary = employee.Salary;
+            Currency = employee.Currency;
+        }
+
+        ~Employee(){}
+        
+        void setEmployeeId(int employeeId){
+            EmployeeId = employeeId;
+        }
+        void setName(string name){
+            Name = name;
+        }
+        void setRatePerHour(float ratePerHour){
+            RatePerHour = ratePerHour;
+        }
+        void setSalary(float salary){
+            Salary = salary;
+        }
+        void setCurrency(string currency){
+            Currency = currency;
+        }
+
+        int getEmployeeId(){
+            return EmployeeId;
+        }
+        string getName(){
+            return Name;
+        }
+        float getRatePerHour(){
+            return RatePerHour;
+        }
+        float getSalary(){
+            return Salary;
+        }
+        string getCurrency(){
+            return Currency;
+        }
+
+        virtual void CalculateSalary() = 0;
+
+        virtual void CalculateSalary(float, float) = 0;
+
+        virtual void CalculateSalary(float, int ) = 0;
+
+        virtual void Display() = 0;
+
+};
+
+#endif
